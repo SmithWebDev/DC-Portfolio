58aeb163aff4f4b8008552f24061def2564c3bb9
DB Smith <smithwebdd@gmail.com>
2019-01-31 22:48:34 -0500

    Added new and edit buttons

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -27,7 +27,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to @portfolio_item, notice: 'Portfolio was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
         format.html { render :edit }
       end
