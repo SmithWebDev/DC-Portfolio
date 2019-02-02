914cae60627634358e64d7ae148facdf445f7146
DB Smith <smithwebdd@gmail.com>
2019-02-02 04:10:52 -0500

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -4,38 +4,39 @@
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
+# system, you should be using db: schema: load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_055820) do
+ActiveRecord:: Schema.define(version: 2019_02_02_082214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "blogs", force: : cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.integer "status", default: 0
+    t.index   ["slug"], name   : "index_blogs_on_slug", unique: true
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
+  create_table "friendly_id_slugs", force: : cascade do |t|
+    t.string  "slug",           null : false
+    t.integer "sluggable_id",   null : false
+    t.string  "sluggable_type", limit: 50
     t.string "scope"
     t.datetime "created_at"
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+    t.index ["slug", "sluggable_type"],          name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"],  name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
-  create_table "portfolios", force: :cascade do |t|
+  create_table "portfolios", force: : cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.text "body"
@@ -45,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_055820) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "skills", force: :cascade do |t|
+  create_table "skills", force: : cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
