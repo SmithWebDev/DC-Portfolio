480b29ab5aa9a9f8eb9d3b7e2a6e4bf32a67b9e1
DB Smith <smithwebdd@gmail.com>
2019-04-08 12:18:39 -0400

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,7 @@ module DCPortfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    #config.action_controller.permit_all_parameters = true
     config.generators.system_tests = nil
   end
 end
