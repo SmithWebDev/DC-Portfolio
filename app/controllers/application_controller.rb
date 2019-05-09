37c22d7ef4dcaaefbead4bce4f6667cc4a972479
DB Smith <smithwebdd@gmail.com>
2019-05-09 12:23:58 -0400

    completed code to be used in gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,16 @@ class ApplicationController < ActionController::Base
   before_action :set_copyright
 
   def set_copyright
-    @copyright = SmithViewTool::Renderer.copyright "Db Smith", "All Rights Reserved"  
+    @copyright = SmithTreatmentViewTool::Renderer.copyright "The Smith Treatment", "All Rights Reserved"  
   end
 end
 
 
 
-module SmithViewTool
-
+module SmithTreatmentViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
