276e46e7d4fe2e7c959916c5e4f65c1aa542f7ea
Smith <smithwebdd@gmail.com>
2019-09-23 11:34:24 -0400

    updated edit card to show correct padding, added style for forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       (link_to  "Register", new_user_registration_path, class: style) + " ".html_safe +
       (link_to  "Login",    new_user_session_path, class: style)
     else
-      link_to  "Logout", destroy_user_session_path, method: :delete
+      (link_to  "Logout", destroy_user_session_path, method: :delete, class: style)
     end
   end
 
