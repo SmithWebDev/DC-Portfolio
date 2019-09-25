e8df3ad7dd27a3d4bcd5cd7af761e104466e18cb
Smith <smithwebdd@gmail.com>
2019-09-25 13:09:44 -0400

    Fixed login helper method by setting default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper style  #Creation of Ruby Method for ease of use, DRY, and scalability
+  def login_helper style = " "  #Creation of Ruby Method for ease of use, DRY, and scalability
     if current_user.is_a?(GuestUser)
       (link_to  "Register", new_user_registration_path, class: style) + " ".html_safe +
       (link_to  "Login",    new_user_session_path, class: style)
