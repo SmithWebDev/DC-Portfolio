f074b420df455db753196e784207c0dbae422fac
Smith <smithwebdd@gmail.com>
2019-11-21 10:33:36 -0500

    portfolio helper error

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,18 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://via.placeholder.com/#{height}x#{width}"
+  end
+  def portfolio_img img, type
+    #byebug
+    if img
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
+
+#   self.main_image ||= Placeholder.
+#   self.thumb_image ||=Placeholder.image_generator(height: 350, width: 200)
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,7 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
-  include Placeholder
-  validates_presence_of :main_image, :title, :body, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -16,13 +15,5 @@ class Portfolio < ApplicationRecord
     order("position ASC")
   end
 
-  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
-  # Ex:- scope :active, -> {where(:active => true)}
-
-  # after_initialize :set_defaults
-
-  # def set_defaults
-  #   self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-  #   self.thumb_image ||=Placeholder.image_generator(height: 350, width: 200)
-  # end
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
 end
